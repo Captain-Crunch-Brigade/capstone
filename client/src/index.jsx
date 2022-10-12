17db4e8cdfa6885e963fe0f96fca5b2824a77bcb
Kee Wook Lee <keewook2@gmail.com>
2022-10-12 16:30:32 -0700

    fix ReactDOM.render to createRoot (required to use in react 18 or higher

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
