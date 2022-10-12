f9bf570c2c55ecca88db8e15be7d6cc611b748ad
Kee Wook Lee <keewook2@gmail.com>
2022-10-12 16:46:20 -0700

    Add: Routes, 404 pages, and ProductDetails component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ProductDetails from './ProductDetails';
+import NotFound from './NotFound';
 
 const App = function App() {
-  return (<div>Hello World!</div>);
+  return (
+    <Routes>
+      <Route path="/" element={<ProductDetails />} />
+      <Route path="products/:id" element={<ProductDetails />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
