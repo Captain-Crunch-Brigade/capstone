74d3d85edc560fbafa5c8a93b7dbfc7f845dec61
Kee Wook Lee <keewook2@gmail.com>
2022-10-12 00:00:29 -0700

    fix: eslintrc for devdependencies in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,8 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    quotes: ['error', 'single', 'avoid-escape'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
 };
