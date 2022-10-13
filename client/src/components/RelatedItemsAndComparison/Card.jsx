64cf1eb72349fb1c1f2e8c13e0a906623189acd7
Kee Wook Lee <keewook2@gmail.com>
2022-10-13 16:17:02 -0700

    Update: dynamically render Card components in Carousel

--- client/src/components/RelatedItemsAndComparison/Card.jsx
+++ client/src/components/RelatedItemsAndComparison/Card.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const Card = function Card() {
+const Wrapper = styled.div`
+  width: 300px;
+  height: 400px;
+  border: 1px solid grey;
+  margin: 10px 20px 10px 10px;
+`;
+
+const Image = styled.img`
+  width: 300px;
+  height: 300px;
+  border-bottom: 1px solid grey;
+`;
+
+const Category = styled.div`
+  font-size: 12px;
+`;
+
+const Name = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const Price = styled.div`
+  font-size: 10px;
+`;
+
+const Card = function Card({ id }) {
+  const [data, setData] = useState({
+    name: '',
+    category: '',
+    price: 0,
+    ratings: {},
+    thumbnails: [],
+  });
+
+  useEffect(() => {
+    axios.get(`/api/related_items/${id}/info`)
+      .then((results) => {
+        setData((prev) => ({
+          ...prev,
+          name: results.data[0].name,
+          category: results.data[0].category,
+          price: parseInt(results.data[0].default_price, 10),
+          ratings: results.data[2],
+          thumbnails: results.data[3].results,
+        }));
+      });
+  }, [id]);
+
   return (
-    <div>
-      Card Components
-    </div>
+    <Wrapper>
+      <Image src={data.thumbnails[0]?.photos[0]?.thumbnail_url} />
+      <Category>{data.category}</Category>
+      <Name>{data.name}</Name>
+      <Price>{`$${data.price}`}</Price>
+    </Wrapper>
   );
+};
+
+Card.propTypes = {
+  id: PropTypes.number,
+};
+
+Card.defaultProps = {
+  id: null,
 };
 
 export default Card;
--- client/src/components/RelatedItemsAndComparison/Carousel.jsx
+++ client/src/components/RelatedItemsAndComparison/Carousel.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 const CarouselDiv = styled.div`
   border: 1px solid black;
   border-radius: 3px;
+  display: flex;
+  flex-direction: row;
 `;
 
-const Carousel = function Carousel() {
+const Carousel = function Carousel({ relatedItems }) {
   return (
     <CarouselDiv>
-      <Card />
-      <Card />
+      {relatedItems?.related_items
+        ? relatedItems.related_items.map((item) => <Card id={item} key={item} />)
+        : null }
     </CarouselDiv>
   );
+};
+
+Carousel.propTypes = {
+  relatedItems: PropTypes.shape({
+    id: PropTypes.number,
+    related_items: PropTypes.array,
+  }),
+};
+
+Carousel.defaultProps = {
+  relatedItems: {
+    id: null,
+    related_items: [],
+  },
 };
 
 export default Carousel;
