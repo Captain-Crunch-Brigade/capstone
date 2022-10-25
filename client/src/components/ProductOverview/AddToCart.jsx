e6bb379e26bf697a61f056c3535502b8007c1bcb
Robert Garcia <robertvincentgarciasr@gmail.com>
2022-10-25 11:36:09 -0700

    create style selector and add to cart components

--- client/src/components/ProductOverview/AddToCart.jsx
+++ client/src/components/ProductOverview/AddToCart.jsx
@@ -5,7 +5,23 @@ const AddToCart = () => {
 
   return (
     <div id={s.cart}>
-      <h1>AddToCart</h1>
+      <select id={s.size}>
+        <option>SELECT SIZE</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+      </select>
+      <select id={s.qty}>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+      </select>
     </div>
   )
 }
