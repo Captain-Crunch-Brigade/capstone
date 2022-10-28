35fa4b48737ef6f4b96edcabee65846e02464dcb
Robert Garcia <robertvincentgarciasr@gmail.com>
2022-10-28 14:54:40 -0700

    add click event to thumbnails

--- client/src/components/ProductOverview/StyleSelector.jsx
+++ client/src/components/ProductOverview/StyleSelector.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import Thumbnails from './Thumbnails'
 import s from './style.css';
 
-const StyleSelector = ({selector}) => {
+const StyleSelector = ({selector, styleId, styleClick}) => {
+
+  let styleName = selector[0]?.name;
+
+  selector.map(style => {
+    if (style.style_id === styleId) {
+      styleName = style.name;
+    }
+  })
+
 
   return (
     <div id={s.selector}>
-      <h4>Style: {selector[0]?.name}</h4>
+      <h4>Style: {styleName}</h4>
       {
         selector.map(item => (
-            <Thumbnails key={item.style_id} item={item} />
+            <Thumbnails key={item.style_id} item={item} styleClick={styleClick} />
         ))
           }
     </div>
